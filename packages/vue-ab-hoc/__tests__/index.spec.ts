05b74b3e7830651a14a716f1e2b49ae27f4b41a4
AuSjukwa <ausjukwa@qq.com>
2024-02-07 02:47:03 +0800

    test(vue-ab-hoc): delete unused variable

--- packages/vue-ab-hoc/__tests__/index.spec.ts
+++ packages/vue-ab-hoc/__tests__/index.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { config, mount } from '@vue/test-utils';
 import { h, nextTick, ref } from 'vue';
 import { type ABKey, WithAbHocPlugin, abHocGenerator, asyncComponent, withAbHoc } from '../src';
